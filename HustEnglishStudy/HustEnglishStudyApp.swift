24d2ebb6e96566db7cf675cb92ad5ce15acfc7a3
Nguyễn Quang Anh <anh.nguyenquang24052004@gmail.com>
2025-08-02 07:29:13 +0700

    Initial Commit

---
+++ HustEnglishStudy/HustEnglishStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HustEnglishStudyApp.swift
+//  HustEnglishStudy
+//
+//  Created by Nguyễn Quang Anh on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct HustEnglishStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
